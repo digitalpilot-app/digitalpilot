ec389000975b617e5a05dbc5feec94ef500f0cb4
Valeriu Tihai <valeriu@tihai.md>
2024-04-19 09:25:27 -0400

    ✨ ADD:  new contributor

--- digitalpilot.php
+++ digitalpilot.php
@@ -10,7 +10,7 @@
  * Requires PHP: 7.4
  * Author: DigitalPilot
  * Author URI: https://digitalpilot.app/
- * Contributors: valeriutihai
+ * Contributors: iamjeylabrecque, valeriutihai
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
